a53e2a7bf5c18a493f338fb00ce181d0e6de185e
zherdeva <irina@nimdas-iMac.local>
2016-05-30 19:31:41 +0300

    5zadanie ispravleno

--- build/js/check.js
+++ build/js/check.js
@@ -1,23 +1,23 @@
 function getMessage(a,b) {
-if (typeof a == 'boolean') {
+if (typeof a === 'boolean') {
 if (a) {
   return "Я попал в " + b;
 } else {
 return "Я никуда не попал";
 }
 }
-if (typeof a == 'number') {
+if (typeof a === 'number') {
   return "Я прыгнул на " + a * 100 + " сантиметров";
 }
 var sum = 0;
-if (typeof a == 'object' && typeof b != 'object') {
+if (typeof a === 'object'&& typeof b != 'object') {
   for (var i=0; i<a.length; i++) {
     sum += a[i];
   }
   return "Я прошёл " + sum + " шагов";
 }
 var length=0;
-if (typeof a == 'object' && typeof b == 'object') {
+if (typeof a === 'object'&& typeof b === 'object') {
   for (var i=0; i<a.length; i++) {
     length += a[i]*b[i];
   }
